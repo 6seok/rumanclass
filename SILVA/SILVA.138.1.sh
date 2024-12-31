178625a0935e5d234d1f790e7ee99653a5304efa
6seok <153567617+6seok@users.noreply.github.com>
2024-12-31 18:41:36 +0900

    SILVA.138.1.sh

--- SILVA/SILVA.138.1.sh
+++ SILVA/SILVA.138.1.sh
@@ -1,5 +1,6 @@
-#!/bin/bash/
+#!/bin/bash
 
+##################################################################################
 qiime rescript get-silva-data \
 --p-version '138.1' \
 --p-target 'SSURef_NR99' \
@@ -26,16 +27,22 @@ qiime rescript dereplicate \
 --o-dereplicated-sequences silva_138.1_SSU_NR99_RNA-seqs_dereplicated_uniq.qza \
 --o-dereplicated-taxa silva_138.1_SSU_NR99_tax_dereplicated_uniq.qza
 
+##################################################################################
+
 qiime feature-classifier fit-classifier-naive-bayes \
 --i-reference-reads silva_138.1_SSU_NR99_RNA-seqs_dereplicated_uniq.qza \
 --i-reference-taxonomy silva_138.1_SSU_NR99_tax_dereplicated_uniq.qza \
 --o-classifier silva_138.1_SSU_NR99_classifier.qza
+
+##################################################################################
 
 qiime feature-classifier classify-sklearn \
 --i-classifier silva_138.1_SSU_NR99_classifier.qza \
 --i-reads merged_sv.qza \
 --p-confidence disable --p-n-jobs 10 \
 --o-classification SILVA_merged_classification.qza
+
+##################################################################################
 
 qiime tools export \
 --input-path SILVA_merged_classification.qza \
@@ -54,12 +61,16 @@ qiime taxa filter-table --i-table merged_processed.qza \
 --p-exclude Chloroplast,Mitochondria \
 --o-filtered-table SILVA_merged_processed_filtered.qza
 
+##################################################################################
+
 qiime clawback generate-class-weights \
 --i-reference-sequences silva_138.1_SSU_NR99_RNA-seqs_dereplicated_uniq.qza \
 --i-reference-taxonomy silva_138.1_SSU_NR99_tax_dereplicated_uniq.qza \
 --i-samples SILVA_merged_processed_filtered.qza \
 --i-taxonomy-classification SILVA_merged_classification_filtered.qza \
 --o-class-weight SILVA_merged_weighted.qza
+
+##################################################################################
 
 qiime feature-classifier fit-classifier-naive-bayes \
 --i-reference-reads silva_138.1_SSU_NR99_RNA-seqs_dereplicated_uniq.qza \
